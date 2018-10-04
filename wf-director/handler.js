45f731760831e169884f7f085e8c6af94bffe2b0
Alex Ellis <alexellis2@gmail.com>
2018-10-04 21:50:30 +0300

    Remove nesting from Promises
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- wf-director/handler.js
+++ wf-director/handler.js
@@ -3,29 +3,30 @@
 const request = require('request-promise-native');
 
 module.exports = (context, callback) => {
-    var gw = process.env.gateway_url || "http://gateway:8080/function/";
+    var gw = process.env.gateway_url || "http://gateway:8080/function/"; 
     let input = JSON.parse(context);
 
     // Would suggest await/async or using the async library and a pipeline
-    request.get(make(gw, "task-1", input))
-    .then(res1 => {
-        console.error(`task-1 - ${res1}`)
-
-        request.get(make(gw, "task-2", res1))
-        .then(res2 => {
-            console.error(`task-2 - ${res2}`)
-
-            request.get(make(gw, "task-3", res2))
-            .then(res3 => {
-                console.error(`task-3 - ${res3}`)
-
-                callback(undefined, res3);
-            })
-        })
+    makeTaskCall(gw, "task-1", input)()
+    .then( res1 => {
+        return res1
+    })
+    .then(res1 => makeTaskCall(gw, "task-2", res1)())
+    .then(res2 => makeTaskCall(gw, "task-3", res2)())
+    .then(finalRes => {
+        callback(undefined, finalRes);
     })
     .catch(e => {
         callback(e);
     });
+}
+    
+var makeTaskCall = function(gw, url, input) {
+    return function() {
+        const urlVal = url;
+        const inputVal = input;
+        return request.get(make(gw, urlVal, inputVal));
+    };
 }
 
 function make(url, fn, body) {
